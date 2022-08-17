21897c20a46aa32c4b208ac68f72a9095dc7f998
Vasilii <106672347+Paci-Vasilii@users.noreply.github.com>
2022-08-17 17:14:17 +0300

    Update TrafficLightDemo.java

--- TrafficLightDemo.java
+++ TrafficLightDemo.java
@@ -3,7 +3,7 @@ package com.company;
 enum TrafficLightColor {
     RED(10000), GREEN(10000), YELLOW(2000);
 
-    private int time;
+    private int time1;
 
     TrafficLightColor(int t) {
         time = t;
